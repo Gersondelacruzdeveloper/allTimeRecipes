b333295aeb07bb59e4e5f1ef0251a1405583662b
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-05-29 12:21:30 +0100

    Add virtual enviroment pipenv

--- allTimeRecipes/routes.py
+++ allTimeRecipes/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from allTimeRecipes import app, db
+from allTimeRecipes.models import User, Recipes, Comments
 
 @app.route("/")
 def home():
     return render_template('home.html')
-    
